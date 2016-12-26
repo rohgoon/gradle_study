f523e5db6aa85a5bd241e2fe5ad851e42139149c
NCG <NCG@DESKTOP-T6ENAEQ>
2016-12-26 16:11:59 +0900

    gradle 설정 및 jar파일과 zip 파일 src/dist/ReadMe.txt 생성

---
+++ src/main/java/kr/co/dgit/school/TeatMain.java
@@ -0,0 +1,11 @@
+package kr.co.dgit.school;
+
+import javax.swing.JFrame;
+
+public class TeatMain {
+	public static void main(String[] args) {
+		JFrame jf = new JFrame();
+		jf.setTitle("test");
+		jf.setVisible(true);
+	}
+}
